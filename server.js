ce9388d8049934cded9ca1fb502419dd47e97353
AnKulai <116018047+AnKulai@users.noreply.github.com>
2023-04-20 16:09:40 +0300

    finished_hw-3

--- server.js
+++ server.js
@@ -1,5 +1,19 @@
-const app = require('./app')
+import * as dotenv from "dotenv"; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+import mongoose from "mongoose";
+import app from "./app.js";
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+dotenv.config();
+const HOST = process.env.DB_HOST;
+
+mongoose
+  .connect(HOST)
+  .then(() => {
+    console.log("Database connection successful");
+    app.listen(3000, () => {
+      console.log("Server running. Use our API on port: 3000");
+    });
+  })
+  .catch((err) => {
+    console.log(err.message);
+    process.exit(1);
+  });
